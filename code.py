aec639c56524f42ecba5450199771d9e5aa8326d
Aysegul-Sen <aaysegulsenn@gmail.com>
2020-11-09 17:49:04 +0300

    video framing is done

--- code.py
+++ code.py
@@ -3,21 +3,21 @@ import os
 import numpy as np
 
 def framing(videofilename):
-    directoryName = videofilename[0:-4]+"_Frames"
-    if (os.path.isdir(directoryName) == False): #this video's directory does not exist. Create one.
+    directoryName = videofilename[0:-4]+"_Frames" #directory name for the video
+    if (os.path.isdir(directoryName) == False): #the video's directory does not exist. Create one.
         os.mkdir(directoryName)
 
-    capture = cv2.VideoCapture(videofilename)
+    capture = cv2.VideoCapture(videofilename) #create VideoCapture object
 
-    number = 0
+    number = 0 #will be used when saving frames as jpg files
     while(capture.isOpened()):
-        number += 1
+        number += 1 #number of frames
         returnValue, imageFrame = capture.read()
         if returnValue: # frame read correctly
             name = directoryName+"/frame#"+str(number)+".jpg"
-            print("Creating " + name)
-            cv2.imwrite(name, imageFrame)
-        else:
+            print("Creating " + name) #track the process
+            cv2.imwrite(name, imageFrame) #save the frame as frame#N.jpg
+        else: #when there is no frame left
             break
 
 
